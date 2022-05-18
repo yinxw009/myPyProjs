c1657a8b66efcbd90fecab876f2f70782da3aa17
yinxw009 <wandy_yxw@126.com>
2022-05-18 21:51:58 +0800

    Update stocks and func, class.@20220516

---
+++ myProjs/myStocks/myStocks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+class Security:
+    def __init__(self, no, name = 'unnamed') -> None:
+        self.no = no
+        self.name = name
+    def setPrc(self, price):
+        self.price = price
+
+    def 
+
+
+class Strock(Security):
+    pass
+
+class Fund(Security):
+    pass
+
+secETF = Fund(512880)
